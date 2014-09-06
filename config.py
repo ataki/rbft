1f5d4c5cef7cfd7592b61e3043bede1c86e10a44
Jim <jzheng@salesforce.com>
2014-09-06 16:25:32 -0700

    Deploy requires something else

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ import os
 DEBUG = True
 SECRET_KEY = '1234' # make sure to change this
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://localhost:''@localhost/test'
-# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://localhost:''@localhost/test'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
