f2180c68209a401a3d2cdf73ab10810a0295482b
Jim <jzheng@salesforce.com>
2014-09-06 16:35:55 -0700

    Model, single log post, and group log post

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
+import simplejson as json
 
 try:
     from flask.ext.cors import CORS  # The typical way to import flask-cors
@@ -33,10 +34,31 @@ class Log(db.Model):
 # cross origin.
 CORS(app, resources=r'/api/*', headers='Content-Type')
 
+API_KEY = "3da541559918a808c2402bba5012f6c60b27661c"
 
 @app.route("/api/v1/users", methods=['GET'])
 def list_users():
     return jsonify({"results": [{"name": "anon"}]})
+
+
+@app.route("/api/v1/log", methods=['POST'])
+def post_log():
+    if request.args.get("apikey") == API_KEY:
+        args = request.args.copy()
+        args.pop("apikey")
+        log = Log(**args)
+        db.session.add(log)
+        db.session.commit()
+
+
+@app.route("/api/v1/log", methods=['POST'])
+def post_logs():
+    if request.args.get("apikey") == API_KEY:
+        log_data = json.loads(request.data)
+        for args in log_data:
+            log = Log(**args)
+            db.session.add(log)
+        db.session.commit()
 
 
 if __name__ == "__main__":
