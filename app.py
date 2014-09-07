2b30550eb7cdf0dc6dd59efb0953f99c835792c7
Jim <jzheng@salesforce.com>
2014-09-06 17:46:03 -0700

    Get timezones working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import simplejson as json
+from datetime import datetime
+from dateutil import parser as dateParser
 
 try:
     from flask.ext.cors import CORS  # The typical way to import flask-cors
@@ -21,10 +23,10 @@ db.create_all()
 
 
 class Log(db.Model):
-    __tablename__ = 'log'
+    __tablename__ = '_rbft_log_'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime(timezone=True), nullable=False)
     happy = db.Column(db.Boolean)
     measure = db.Column(db.Float)
 
@@ -32,9 +34,11 @@ class Log(db.Model):
 # One of the simplest configurations. Exposes all resources matching /api/* to
 # CORS and allows the Content-Type header, which is necessary to POST JSON
 # cross origin.
-CORS(app, resources=r'/api/*', headers='Content-Type')
+CORS(app, resources={r"/api/*": {'origins': '*'}}, headers="Content-Type")
+
 
 API_KEY = "3da541559918a808c2402bba5012f6c60b27661c"
+
 
 @app.route("/api/v1/users", methods=['GET'])
 def list_users():
@@ -44,11 +48,12 @@ def list_users():
 @app.route("/api/v1/log", methods=['POST'])
 def post_log():
     if request.args.get("apikey") == API_KEY:
-        args = request.args.copy()
-        args.pop("apikey")
+        args = json.loads(request.data)
+        args['timestamp'] = dateParser.parse(args['timestamp']).replace(tzinfo=None)
         log = Log(**args)
         db.session.add(log)
         db.session.commit()
+    return jsonify({"results": [{"name": "anon"}]})
 
 
 @app.route("/api/v1/log", methods=['POST'])
@@ -56,9 +61,11 @@ def post_logs():
     if request.args.get("apikey") == API_KEY:
         log_data = json.loads(request.data)
         for args in log_data:
+            args['timestamp'] = dateParser.parse(args['timestamp']).replace(tzinfo=None)
             log = Log(**args)
             db.session.add(log)
         db.session.commit()
+    return jsonify({"results": [{"name": "anon"}]})
 
 
 if __name__ == "__main__":
